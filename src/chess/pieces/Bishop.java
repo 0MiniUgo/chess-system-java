4ddd275ea2bdf00512cd1b8285c9d951540ae73d
0MiniUgo <hugorodrigues969@gmail.com>
2022-12-06 16:49:27 -0300

    Bishop

--- src/chess/pieces/Bishop.java
+++ src/chess/pieces/Bishop.java
@@ -5,15 +5,15 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Rook extends ChessPiece {
+public class Bishop extends ChessPiece {
 
-    public Rook(Board board, Color color) {
+    public Bishop(Board board, Color color) {
         super(board, color);
     }
 
     @Override
     public String toString() {
-        return "R";
+        return "B";
     }
 
     @Override
@@ -22,10 +22,10 @@ public class Rook extends ChessPiece {
 
         Position p = new Position(0,0);
 
-        for(int i = -1; i< 2; i++){
-            for(int j = -1;j < 2; j++){
+        for(int i = -1; i < 2; i++){
+            for(int j = -1; j < 2; j++){
 
-                if((i == 0 && j == 0) || (i != 0 && j != 0)) continue;
+                if((i == 0 || j == 0)) continue;
 
                 p.setValues(position.getRow() + i, position.getColumn() + j);
 
